5f0896fcc83c31b78930701b14880b5270564872
hongsokan_mac <hongsokan@daum.net>
2023-06-22 17:36:57 +0900

    add samples

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-native-boiler-plate-app
+
+> 
+[따라하며 배우는 리액트 네이티브 기초](https://www.inflearn.com/course/%EB%94%B0%EB%9D%BC%ED%95%98%EB%A9%B0-%EB%B0%B0%EC%9A%B0%EB%8A%94-%EB%A6%AC%EC%95%A1%ED%8A%B8-%EB%84%A4%EC%9D%B4%ED%8B%B0%EB%B8%8C) 해당 강의를 참고하여 만든 리액트 네이티브 앱 boiler-plate
+
+## How to start
+```
+npm install && npm start
+```
+
---
+++ screens/MainScreen.js
@@ -0,0 +1,21 @@
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainScreen = () => {
+    return (
+        <SafeAreaView>
+            <StatusBar></StatusBar>
+            <Text>Todo App</Text>
+            <View>
+                <Text>할 일</Text>
+            </View>
+            <View>
+                <Text>완료된 일</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default MainScreen
+
+const styles = StyleSheet.create({})
